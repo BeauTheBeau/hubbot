b3667d1e04276748a29900eec86ef5b99486b59e
beauthebeau <beauthebeau@proton.me>
2023-09-18 22:03:36 +0100

    lebron

--- src/events/messages/messageCreate.js
+++ src/events/messages/messageCreate.js
@@ -1,4 +1,5 @@
 const userModel = require('../../models/userModel.js');
+const {EmbedBuilder} = require("discord.js");
 
 module.exports = {
     name: 'messageCreate',
@@ -12,6 +13,33 @@ module.exports = {
                 lastUpdated: Date.now()
             });
             await newUser.save();
+        }
+
+        if (message.author.id === "756591292057911446") {
+
+            console.log(message.content);
+
+            // Send message in channel with ID 1153431596171526245
+            const channel = await client.channels.cache.get('1153434368543559700');
+            if (!channel) return;
+
+            const embed = new EmbedBuilder()
+                .setURL(message.url)
+                .setTitle('Big Lebron Quotations')
+                .setDescription(message.content)
+                .setThumbnail(message.author.avatarURL())
+
+            const sentMessage = await channel.send({embeds: [embed]});
+
+            // react with epic, positive emoji
+            await sentMessage.react('🤩');
+            await sentMessage.react('👍');
+            await sentMessage.react('👌');
+            await sentMessage.react('👏');
+            await sentMessage.react('🙌');
+            await sentMessage.react('👑');
+            await sentMessage.react('🐐');
+
         }
 
     }
